7a2490121594a73c4e74f945b8f7760265bdaddb
Sabir Ali <sabir009ali@email.com>
2026-05-12 15:54:40 +0530

    Episode 2 - Igniting our app

--- App.js
+++ App.js
@@ -1,35 +1,22 @@
-// const heading = React.createElement(
-//   "h1",
-//   { id: "heading" },
-//   "Hello from React Js!",
-// );
-// console.log(heading); //Object
+import React from "react";
+import { createRoot } from "react-dom/client";
 
-//nested div
-{
-  /* <div id="parent">
-  <div id="child">
-    <h1>This is a h1 tag</h1>
-  </div>
-</div>; */
-}
-
-const parent = React.createElement("div", { id: "parent" }, [
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
   React.createElement(
     "div",
     { id: "child1" },
     React.createElement("h1", {}, "This is an h1 tag"),
-    React.createElement("h2", {}, "This is an h2 tag"),
+    React.createElement("h2", {}, "This is an h2 tag element"),
   ),
-  React.createElement("div", { id: "child2" }, [
+  React.createElement(
+    "div",
+    { id: "child2" },
     React.createElement("h1", {}, "This is an h1 tag"),
     React.createElement("h2", {}, "This is an h2 tag"),
-  ]),
-]);
+  ),
+);
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  parent,
-); /*render function converts  JS Object(heading) to js code that can browser understand and show it into div root
-or  render() displays React elements inside the root element by converting virtual DOM into real DOM.`
-*/
+const root = createRoot(document.getElementById("root"));
+root.render(parent);
---
+++ README.md
@@ -0,0 +1,19 @@
+# parcel
+
+- development build
+- Local server
+- HMR = hot module replacement
+- File Watching Algorithm - written in c++
+- Caching - faster builds
+- Image optimization
+- minification
+- bundling
+- compress
+- consistent hashing
+- code splitting
+- differential Bundling - support older browsers
+- Diagnostic
+- Error Handling
+- HTTPs
+- Tree Shaking - remove unused code
+- Different dev and prod bundles
